48f2f152adb90495a3e7c3dc46a1216bb008f845
nikunjbhardwaj <86513219+nikunjbhardwaj@users.noreply.github.com>
2026-02-19 15:25:42 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,10 @@ export default function Home() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "http://localhost:3000/auth/callback",
+        redirectTo: `${window.location.origin}/auth/callback`,
       },
     });
+
   };
 
 
